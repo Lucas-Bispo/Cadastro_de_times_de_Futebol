a803447595632db7c203b164eeae539de680abb1
Lucas-Bispo <lukaobispo@gmail.com>
2020-09-16 09:25:58 -0300

    Erro de imporatação resolver no web/main.dart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,18 +1,14 @@
 import 'package:angular/angular.dart';
-
-import 'src/todo_list/todo_list_component.dart';
-
+import 'src/app_times/app_times_component.dart';
 // AngularDart info: https://angulardart.dev
 // Components info: https://angulardart.dev/components
 
-@Component( //meta dados
-  selector: 'my-app',
-  styleUrls: ['app_component.css'],//diz onde está o css
-  templateUrl: 'app_component.html',//template
-  directives: [TodoListComponent],
+@Component(
+  selector: 'app-times',
+  styleUrls: ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: [AppTimesComponent],
 )
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+  // Nothing here yet. All logic is in AppTimesComponent.
 }
-
-
